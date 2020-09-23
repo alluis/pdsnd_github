f20ffa504a2ff93a06d2ed74788f639351d44bd4
ALVARO LLUIS <alluis1982@gmail.com>
2020-09-23 22:53:58 +0200

    Update in bikeshare

--- bikeshare.py
+++ bikeshare.py
@@ -1,7 +1,7 @@
 import time
 import pandas as pd
 import numpy as np
-# We create some variables
+# We create some variables. change made for version control project
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
